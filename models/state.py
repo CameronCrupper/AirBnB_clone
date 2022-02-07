722d5ba42584e61d951bfc50bfc4f92e21c04cf0
Cameron Crupper <guitardude20795@yahoo.com>
2022-02-07 11:58:46 -0600

    test

--- models/state.py
+++ models/state.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+"""
+State Module
+Inherits from BaseModel
+Has attributes for States
+"""
+
+
+from models.base_model import BaseModel
+
+
+
+class State(BaseModel):
+    """
+    Class State
+    name (string): State Name
+    """
+    name = ''
